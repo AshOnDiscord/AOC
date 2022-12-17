8c53f4ac7bf86cf465c63aa17c6c620349f95ecd
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2022-12-16 22:53:18 -0500

    Day 4 - 2022 | JS/TS

---
+++ 2022/TS_JS/Day_4/part1.ts
@@ -0,0 +1,30 @@
+import { readFileSync } from "fs";
+
+// Load and read input into a string
+const input: string = readFileSync("./input.txt", "utf8");
+
+const pairs: string[] = input.split("\n").filter((e) => e);
+
+const elves: string[][] = pairs.map((e) => e.split(","));
+
+const ranges: number[][][] = elves.map((e) =>
+  e.map((k) => k.split("-").map((l) => parseInt(l)))
+);
+
+let sum = 0;
+
+for (const pair of ranges) {
+  const elf1 = pair[0];
+  const elf2 = pair[1];
+
+  if (elf1[0] <= elf2[0] && elf1[1] >= elf2[1]) {
+    sum += 1;
+    continue;
+  }
+  if (elf2[0] <= elf1[0] && elf2[1] >= elf1[1]) {
+    sum += 1;
+    continue;
+  }
+}
+
+console.log(sum);
