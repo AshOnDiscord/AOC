f72573e03a460f6b55784e1363545e5678f0ce0c
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2023-12-03 03:28:03 -0500

    Day 3, Part 1 - JS - 2023

---
+++ 2023/js/day3-gear-ratios-p1.ts
@@ -0,0 +1,68 @@
+const input = `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+const symbols = "*@#$%&-/=+".split("")
+
+const charArr = input.trim().split("\n").map(e => e.trim().split(""))
+
+interface char {
+    y: number,
+    x: number,
+    v: string
+}
+
+let partsSum = 0;
+
+for (let i = 0; i < charArr.length; i++) {
+    let nums: char[][] = []
+    for (let j = 0; j < charArr[i].length; j++) {
+        const c = charArr[i][j]
+        if (!isNaN(parseInt(c))) {
+            if (nums.length === 0) {
+                nums.push([])
+            }
+            nums[nums.length - 1].push({ y: i, x: j, v: c})
+        } else {
+            if (nums.length !== 0 && nums[nums.length - 1].length !== 0) {
+                nums.push([])
+            }
+        }
+    }
+    nums = nums.filter(e => e.length !== 0)
+    // console.log(nums.map(e => e.map(a => a.v).join("")))
+
+    const parts: number[] = []
+    numLoop: for (const num of nums)  {
+        for (const c of num) {
+            const offsets = [
+                [-1, -1], [-1,  0], [-1,  1], 
+                [ 0, -1],           [ 0,  1],
+                [ 1, -1], [ 1,  0], [ 1,  1]
+            ]
+            for (const offset of offsets)  {
+                const newY = c.y + offset[0];
+                const newX = c.x + offset[1];
+                if (newY < 0 || newY > charArr.length - 1 || newX < 0 || newX >  charArr.length - 1) continue
+                // console.log(num.map(e=>e.v).join(""), c.v, charArr[newY][newX], offset)
+                // if in bounds
+                if (symbols.includes(charArr[newY][newX])) {
+                    // if adjacent
+                    parts.push(parseInt(num.map(e => e.v).join("")))
+                    continue numLoop;
+                } 
+            }
+        }
+    }
+    if (nums.length !== 0)  console.log(i, nums.map(a =>a.map(e=>e.v).join("")), parts)
+    parts.forEach(e => partsSum += e)
+}
+console.log(partsSum)
